aac631020879b471b0f3716428c7d5860689e5e5
ocpcps <atendimento@ocp.inf.br>
2024-07-26 22:16:48 -0300

    Inicio de projeto: Osiris em 26/07/2024 V.1.0.12

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing.module';
-import { provideHttpClient } from '@angular/common/http';
-import { KeycloakService } from 'keycloak-angular';
-import { APP_INITIALIZER } from '@angular/core';
 
-function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
-      config: 'assets/keycloak.json',
-      initOptions: {
-        onLoad: 'login-required',
-        checkLoginIframe: false
-      },
-      enableBearerInterceptor: true,
-      bearerExcludedUrls: ['/assets']
-    });
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    KeycloakService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService]
-    }
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
